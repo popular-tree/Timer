adaa3866897efbaa04f3ccf5e5747b8b3246e868
포플러나무 <164111625+popular-tree@users.noreply.github.com>
2025-07-28 16:19:42 +0900

    타이틀 추가

--- timer.py
+++ timer.py
@@ -6,3 +6,13 @@ st.set_page_config(
     page_icon='⏱️',
     layout='centered'
 )
+
+# st.title('⏱️타이머')
+# st.caption('작업 리듬을 만들어주는 음악 타이머')
+
+st.markdown("""
+<div style="text-align: center; margin: 2rem 0;">
+    <h1 style="font-size: 3rem; font-weight: bold;">⏱️타이머</h1>
+    <p style="color: #888; font-size: 0.8rem;">작업 리듬을 만들어주는 음악 타이머</p>
+</div>
+""", unsafe_allow_html=True)
