1d72b6fed22dd1258061272e1eeaf9c6f595a34e
포플러나무 <164111625+popular-tree@users.noreply.github.com>
2025-07-28 16:37:44 +0900

    오류 재수정

--- timer.py
+++ timer.py
@@ -89,7 +89,7 @@ with col_left:
             st.markdown('⏱️타이머', help='타이머가 대기중입니다.')
 
     with status_col3:
-        st.markdown(f'{"int(progress*100)"%}')
+        st.markdown(f'{int(progress*100)}%')
 
 with col_right:
     pass
